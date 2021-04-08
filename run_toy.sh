28cf03071793c9b05afc8f9ad36a74d16a9da5d5
skviswa <karthik.kv.mst@gmail.com>
2021-04-08 20:27:36 +0530

    add script for toy training of mediqa

--- run_toy.sh
+++ run_toy.sh
@@ -5,5 +5,6 @@
 # Note that this is a toy setting and please refer our paper for detailed hyper-parameters.
 ############################### 
 
-python prepro.py
-python train.py
+#python prepro.py
+#python train.py
+python train.py --task_def experiments/glue/mediqa_task_def_training.yml --data_dir ../data/canonical_data/bert_large_uncased/ --train_datasets mednli,rqe,mediqa,medquad --test_datasets mednli,rqe,mediqa
